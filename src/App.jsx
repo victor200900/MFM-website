1b8a724727e2a1c55a256c476293d7a9aab160b8
Akpan Victor <akpanvictor456@gmail.com>
2025-11-02 00:41:45 +0100

    update site

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,24 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="flex m-0 p-0 flex-col min-h-screen bg-purple-900 text-gray-100 font-[Poppins]">
+    <div className="flex flex-col min-h-screen bg-purple-900 text-gray-100 font-[Poppins] m-0 p-0 overflow-x-hidden">
       {/* Navbar (always visible) */}
       <Navbar />
 
-      {/* Page Content */}
+      {/* Main content */}
       <main className="flex-grow m-0 p-0">
         <Routes>
           {/* Home Page */}
           <Route
             path="/"
             element={
-              <>
+              <div className="m-0 p-0">
                 <Hero />
                 <About />
                 <Pastors />
                 <Belief />
                 <Contact />
-              </>
+              </div>
             }
           />
 
@@ -37,8 +37,8 @@ function App() {
           <Route
             path="/youth"
             element={
-              <section className="py-24 bg-gradient-to-b from-purple-950 via-purple-900 to-purple-800 min-h-screen">
-                <div className="container mx-auto px-6">
+              <section className="py-24 bg-gradient-to-b from-purple-950 via-purple-900 to-purple-800 min-h-screen m-0 p-0">
+                <div className="container mx-auto px-6 m-0 p-0">
                   <Link
                     to="/"
                     className="inline-block mb-8 text-sm text-pink-400 hover:text-pink-300 transition"
@@ -55,8 +55,8 @@ function App() {
           <Route
             path="/outreach"
             element={
-              <section className="py-24 bg-gradient-to-b from-purple-950 via-purple-900 to-purple-800 min-h-screen">
-                <div className="container mx-auto px-6">
+              <section className="py-24 bg-gradient-to-b from-purple-950 via-purple-900 to-purple-800 min-h-screen m-0 p-0">
+                <div className="container mx-auto px-6 m-0 p-0">
                   <Link
                     to="/"
                     className="inline-block mb-8 text-sm text-pink-400 hover:text-pink-300 transition"
